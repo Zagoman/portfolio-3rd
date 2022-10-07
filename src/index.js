7cbaedc9156b5d2d3e4e4fe91047c7cbfa4bc712
Lucas <lucaszago.ph@gmail.com>
2022-10-07 20:33:43 +0200

    fix projects fit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 "use strict";
 import APP from "./modules/APP";
+import UIAnim from "./modules/UIAnimations";
 
 let _APP = null;
+let _UIAnimations = null;
 document.addEventListener("DOMContentLoaded", () => {
   if (window.innerWidth < 1000) {
     document.body.style.height = window.innerHeight + "px";
@@ -11,4 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("header").style.height = window.innerHeight + "px";
   }
   _APP = new APP();
+  _UIAnimations = new UIAnim();
 });
--- src/modules/UIAnimations.js
+++ src/modules/UIAnimations.js
@@ -2,8 +2,31 @@ import { animate, stagger, spring } from "https://cdn.skypack.dev/motion";
 
 export default class UIAnim {
   constructor() {
-    this.HTML = {};
+    this.HTML = {
+      burgerTrigger: document.querySelector("#burger_trigger"),
+      closeMenuTrigger: document.querySelector(".close-menu_trigger"),
+      menu: document.querySelector("#menu"),
+    };
+
+    this._Init();
   }
 
-  _AnimateMenu() {}
+  _Init() {
+    this.HTML.burgerTrigger.addEventListener("click", () => {
+      this._AnimateMenu();
+    });
+    this.HTML.closeMenuTrigger.addEventListener("click", () => {
+      this._AnimateMenu();
+    });
+  }
+
+  _AnimateMenu() {
+    if (this.HTML.menu.dataset.menu === "closed") {
+      animate(this.HTML.menu, { transform: "translate(0,0)" }, { delay: 0, easing: "ease-in-out" });
+      this.HTML.menu.dataset.menu = "open";
+    } else {
+      animate(this.HTML.menu, { transform: "translate(-100%,0)" }, { delay: 0, easing: "ease-in-out" });
+      this.HTML.menu.dataset.menu = "closed";
+    }
+  }
 }
