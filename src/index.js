4d3fd0b627005b329431350e4a9a3506bd713ade
Lucas <lucaszago.ph@gmail.com>
2022-10-07 16:48:55 +0200

    js to fix vh on mobile

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import APP from "./modules/APP";
 
 let _APP = null;
 document.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth < 1000) {
+    document.body.style.height = window.innerHeight + "px";
+    document.querySelectorAll("section").forEach((section) => {
+      section.style.minHeight = `${window.innerHeight}px`;
+    });
+    document.querySelector("header").style.height = window.innerHeight + "px";
+  }
   _APP = new APP();
 });
---
+++ src/modules/UIAnimations.js
@@ -0,0 +1,9 @@
+import { animate, stagger, spring } from "https://cdn.skypack.dev/motion";
+
+export default class UIAnim {
+  constructor() {
+    this.HTML = {};
+  }
+
+  _AnimateMenu() {}
+}
